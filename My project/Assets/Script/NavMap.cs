77bef99cd8725657cc9c1b79d3dee47e9128f67a
Barubear <zxsyxx@gmail.com>
2023-05-29 17:08:16 +0900

    updata

--- My project/Assets/Script/NavMap.cs
+++ My project/Assets/Script/NavMap.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class NavMap : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject[,] visualMap;
+    public VisualMap VisualMap;
+    GameObject[,] visualMap;
+    public List<GameObject> wallList;
     float[,] navMap;
     void Start()
     {
+        visualMap = VisualMap.visualMap;
         navMap = new float[visualMap.GetLength(0), visualMap.GetLength(1)];
-    }
+        for (int w = 0; w < visualMap.GetLength(0); w++)
+        {
 
+            for (int h = 0; h < visualMap.GetLength(1); h++)
+            {
+                Bounds cellBound = visualMap[w, h].GetComponent<Renderer>().bounds;
+                foreach (GameObject b in wallList) {
+                    if (b.GetComponent<Renderer>().bounds.Intersects(cellBound)) {
+                        //Destroy(visualMap[w, h]);
+                        navMap[w,h] = -1;
+                        //Debug.Log("!");
+                    }
+                }
+
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
